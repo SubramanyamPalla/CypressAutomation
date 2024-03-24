12fcc7751dce20550bd5f6802d0e84bd4c42d5e5
Subramanyam Palla <Subramanyam.Palla@interflora.co.uk>
2024-03-24 12:07:38 +0530

    Updated origin session details & Practice Task details

---
+++ CypressAutomation/cypress/e2e/Origin.cy.js
@@ -0,0 +1,46 @@
+
+describe('Multiple domains', () => {
+
+    it('origin', () => {
+
+        cy.viewport(1920, 1024)
+        //Navigate to google site--First Origin
+
+        cy.visit('https://www.google.com/')
+
+        //Search git hub 
+
+        cy.get('.gLFyf').first().type('github {enter}')
+
+        cy.get('[jsname="UWckNb"]').first().click()
+        //Click on github link--2nd origin- https://github.com/
+
+        cy.origin('https://github.com', () => {
+
+            //Assertion
+
+            cy.get('[href="/login"]').should('be.visible')
+
+            //CLick on sign in link on github page
+
+            cy.get('[href="/login"]').eq(1).click()
+
+
+        })
+    })
+
+    it("Implement given scenarios in below websit", () => {
+        cy.visit('https://automationexercise.com/')
+
+
+    })
+
+    it("Dummy payment cards details", () => {
+
+        cy.visit('https://support.bluesnap.com/docs/test-credit-card-numbers')
+
+
+    })
+
+
+})
